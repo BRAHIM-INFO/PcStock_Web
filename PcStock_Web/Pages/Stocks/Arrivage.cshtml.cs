a9e39d960e3c01b16e6bc2ffc2a113944a9f655d
IBRAHIM BENYOUNES <benyounesibrahim@hotmail.com>
2026-03-02 06:17:51 +0000

    First

---
+++ PcStock_Web/Pages/Stocks/Arrivage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PcStock_Web.Pages.Stocks
+{
+    public class ArrivageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
